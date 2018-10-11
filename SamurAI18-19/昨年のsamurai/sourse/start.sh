26ab40393bf98f5835d56f1c40fc17bbedb28e6d
U-DESKTOP-56ETH07\KentoTanaka <19930815k@gmail.com>
2018-10-11 18:29:11 +0900

    ベース環境制作

---
+++ SamurAI18-19/昨年のsamurai/sourse/start.sh
@@ -0,0 +1,11 @@
+player1="player/jocky"
+name1="jocky1"
+player2="player/jocky"
+name2="jocky2"
+for var in 01 02 03 04 05 06 07 08 09 10 
+do
+	./official/official samples/course${var}.smrjky $player1 $name1 $player2 $name2 --stdinLogFile0 playerLog/stdin0.txt --stdinLogFile1 playerLog/stdin1.txt --stderrLogFile0 playerLog/stderr0.txt --stderrLogFile1 playerLog/stderr1.txt > gameLog/${var}-1.json
+	./official/official samples/course${var}.smrjky $player2 $name2 $player1 $name1 --stdinLogFile0 playerLog/stdin0.txt --stdinLogFile1 playerLog/stdin1.txt --stderrLogFile0 playerLog/stderr0.txt --stderrLogFile1 playerLog/stderr1.txt > gameLog/${var}-2.json
+done
+./official/official samples/sample-course.smrjky $player1 $name1 $player2 $name2 --stdinLogFile0 playerLog/stdin0.txt --stdinLogFile1 playerLog/stdin1.txt --stderrLogFile0 playerLog/stderr0.txt --stderrLogFile1 playerLog/stderr1.txt > gameLog/sample-1.json
+./official/official samples/sample-course.smrjky $player2 $name2 $player1 $name1 --stdinLogFile0 playerLog/stdin0.txt --stdinLogFile1 playerLog/stdin1.txt --stderrLogFile0 playerLog/stderr0.txt --stderrLogFile1 playerLog/stderr1.txt > gameLog/sample-2.json
