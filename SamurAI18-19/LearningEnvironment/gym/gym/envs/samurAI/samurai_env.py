4288cf7435a848bd40bd11bf676efa658b5efaa2
U-DESKTOP-56ETH07\KentoTanaka <19930815k@gmail.com>
2018-12-06 18:39:46 +0900

    S8-2-1対応

--- SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env.py
+++ SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env.py
@@ -1,5 +1,22 @@
 import numpy as np
 import gym
+import gym.spaces
 
-
+# マップ上のjockyの加速度を操作として、ゴールに移動させることを目標とする環境
 class SamuraiEnv(gym.Env):
+
+	def _init_(self):
+
+		# 加速度9種(-1~1,-1~1)
+		self.action_space = gym.spaces.Discrete(9)
+
+		#観測空間(俺らが見たい世界...jockyのpositionとvelocity)
+		#速度の上限、下限はとりあえず10,-10で(ステージごとに計算したほうがいい？)
+		#positionの上限はルール通り19,99
+		#下限は0,-無限だが、とりあえず、0,-10で
+		high = np.array[[10,10],[19,99]]
+		low = np.array[[-10,-10],[0,-10]]
+		self.observation_space = gym.spaces.Box(low=-high, high=high)
+
+		#_reset呼び出し
+		self.reset()
