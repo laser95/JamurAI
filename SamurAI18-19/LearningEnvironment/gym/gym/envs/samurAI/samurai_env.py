221ec78c3bb1218221a524374c44dd084dd815fa
U-DESKTOP-PUIG48U\yamauchi <s1810187@jaist.ac.jp>
2018-12-08 19:16:46 +0900

    S8-2-1 : observation_spaceをsapces.Distを使って定義するように修正

--- SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env.py
+++ SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env.py
@@ -10,13 +10,19 @@ class SamuraiEnv(gym.Env):
 		# 加速度9種(-1~1,-1~1)
 		self.action_space = gym.spaces.Discrete(9)
 
-		#観測空間(俺らが見たい世界...jockyのpositionとvelocity)
+		#観測空間(俺らが見たい世界...jockyのmapとpositionとvelocity)
+		#mapの値域は[-3,2]
+		#0->None , 1->Obstacle, 2->Puddle, -1->Unknown, -2->Behind Goal, -3->Outside Course
 		#positionの上限はルール通り19,99
 		#下限は0,-無限だが、とりあえず、0,-10で
 		#速度の上限、下限はとりあえず10,-10で(ステージごとに計算したほうがいい？)
-		high = np.array[[19,99],[10,10]]
-		low = np.array[[0,-10],[-10,-10]]
-		self.observation_space = gym.spaces.Box(low,high)
+		#map,position,velocityそれぞれで値域が異なるのでDictionaryを使って定義
+		#
+		self.observation_space = gym.spaces.Dict({
+			"map" : spaces.Box(-3,2,shape=(20,100),dtype="int32"),
+			"position" : spaces.Box(np.array([0,-10]),np.array([19,99]),dtype="int32")),
+			"velocity" : spaces.Box(np.array([-10,-10]),np.array([10,10]),dtype="int32")
+		})
 
 		#_reset呼び出し
 		self.reset()
