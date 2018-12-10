09698170b2fb7a0f7a1be9e6b344d402f701384b
U-DESKTOP-56ETH07\KentoTanaka <19930815k@gmail.com>
2018-12-11 02:51:09 +0900

    S8-2-1修正

--- SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env.py
+++ SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env.py
@@ -10,16 +10,16 @@ class SamuraiEnv(gym.Env):
 		# 加速度9種(-1~1,-1~1)
 		self.action_space = gym.spaces.Discrete(9)
 
-		#観測空間(俺らが見たい世界...jockyのmapとpositionとvelocity)
+		#観測空間(俺らが見たい世界...jockyのmapとlengthとpositionとvelocity)
 		#mapの値域は[-3,2]
 		#0->None , 1->Obstacle, 2->Puddle, -1->Unknown, -2->Behind Goal, -3->Outside Course
 		#positionの上限はルール通り19,99
 		#下限は0,-無限だが、とりあえず、0,-10で
 		#速度の上限、下限はとりあえず10,-10で(ステージごとに計算したほうがいい？)
 		#map,position,velocityそれぞれで値域が異なるのでDictionaryを使って定義
-		#
 		self.observation_space = gym.spaces.Dict({
 			"map" : spaces.Box(-3,2,shape=(20,100),dtype="int32"),
+			"length" : spaces.Box(50,100,dtype="int32")),
 			"position" : spaces.Box(np.array([0,-10]),np.array([19,99]),dtype="int32")),
 			"velocity" : spaces.Box(np.array([-10,-10]),np.array([10,10]),dtype="int32")
 		})
