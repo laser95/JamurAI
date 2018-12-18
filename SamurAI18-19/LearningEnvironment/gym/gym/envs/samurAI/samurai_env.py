d99c6b797fef05f86621544d539d29d7db1587e5
Takuma YAMAUCHI <s1810187@lin01.jaist.ac.jp>
2018-12-18 16:11:23 +0900

    env modified

--- SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env.py
+++ SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env.py
@@ -59,6 +59,7 @@ class SamuraiEnv(gym.Env):
 		next_pos = self._pos + next_vel #予定位置
 
 
+		self._step+=1
 		'''
 			0...none
 			1...obsta
@@ -69,49 +70,55 @@ class SamuraiEnv(gym.Env):
 		           vel = [0,0]
 			→動けない時も上記と同じ
 		'''
+		
+		done = self._is_done(self._pos,next_pos)
+
+		pre_pos=self._pos
 		if self._movable(self._pos,next_pos): 
-			if self.map[next_pos[1]][next_pos[0]] == 0:
+			if next_pos[1]<0 or (not done and self.map[next_pos[1]][next_pos[0]] == 0):
 				self._vel = next_vel
 				self._pos = next_pos
-			if self.map[next_pos[1]][next_pos[0]] == 2:
+			elif not done and self.map[next_pos[1]][next_pos[0]] == 2:
 				self._pos = next_pos
 				self._vel = np.array([0,0])
 		else :
 			self._vel = np.array([0,0])
 			self._pos = self._pos
 
-		done = self._is_done(self._pos,next_pos)
 
 	    	#ゴールしたら1,それ以外はゴールにy軸
 		if done:
-			reward = 1.0
+			reward = 100.0
+			print('\n\n\n!!!!!!goolgoolgool!!!!\n\n\n')
+			
 		else:
 			#reward = 1.0 * self._pos[1]/goolY
-			reward = 1.0 *self._pos[1]/self.length
+			reward = 100.0 *(next_pos[1]-pre_pos[1])/self.length
 
 
-			#取り得る最大のマップを定義, 全て1(壁)で指定
-			big_map = np.empty((100, 20)) 
-
-			#指定されたマップを-1(未知)で指定
-			big_map[0:self.length, 0:self.width] = -1
-
-			#指定されたマップのy軸の範囲外を-2(ゴール以降)で指定
-			big_map[self.length:99, 0:19] = -2
-	
-			#指定されたマップ範囲外を-3(コース外)で指定
-			big_map[:, self.width+1:19] = -3  
-		
-			step = 0 
-			# ゴールするまで、毎stepでbig_mapを取得
-		while self._pos[1] < self.length:
+		#取り得る最大のマップを定義, 全て1(壁)で指定
+		big_map = np.empty((100, 20)) 
+		#指定されたマップを-1(未知)で指定
+		big_map[0:self.length, 0:self.width] = -1
+		#指定されたマップのy軸の範囲外を-2(ゴール以降)で指定
+		big_map[self.length:100, 0:self.width] = -2
+		#指定されたマップ範囲外を-3(コース外)で指定
+		big_map[:, self.width:20] = -3  
+		# ゴールするまで、毎stepでbig_mapを取得
+		if self._pos[1] < self.length:
 			vision = self._pos[1] + self.vision
-			big_map[0:vision-1, 0:self.width] = self.map[0:vision-1, :]
-			step += 1
+			if vision < 0:
+				pass
+			elif vision > self.length:
+				big_map[0:self.length,0:self.width] = self.map
+			else:
+				big_map[0:vision, 0:self.width] = self.map[0:vision,:]
 
+		if self.step_limit <= self._step:
+			done=True		
 
 		#return {"map" : big_map, "length" : self.length, "position" : self._pos, "velocity" : self._vel},reward,done,{}
-		return np.concatenate([big_map.reshape(2000,),self.length.reshape(1,),self._pos.reshape(2,),self._vel.reshape(2,)])
+		return np.concatenate([big_map.reshape(2000,),self.length.reshape(1,),self._pos.reshape(2,),self._vel.reshape(2,)]),reward,done,{}
 
 
 	def reset(self):
@@ -129,9 +136,11 @@ class SamuraiEnv(gym.Env):
 		self.width = json_obj['width']
 		self.length = json_obj['length']
 		self.vision = json_obj['vision']
+		self.step_limit=json_obj['stepLimit']
 		self.startX[0] = json_obj['x0']
 		self.startX[1] = json_obj['x1']
 		self.map = np.reshape(json_obj['squares'], (self.length, self.width))
+		self._step=0
 		#視界限界変数に値格納
 		#生成されたコースから、スタート地点のx軸計算
 
@@ -151,21 +160,25 @@ class SamuraiEnv(gym.Env):
 
 		big_map = np.empty((100, 20))
 		#指定されたマップのy軸の範囲外を-2(ゴール以降)で指定
-		big_map[self.length:99, 0:19] = -2	
+		big_map[self.length:100, 0:20] = -2	
 		#指定されたマップ範囲外を-3(コース外)で指定
-		big_map[:, self.width+1:19] = -3
+		big_map[:, self.width:20] = -3
+		vision = self._pos[1] + self.vision
+		big_map[0:vision, 0:self.width] = self.map[0:vision,:]
 		big_map[0:self.length,0:self.width]=self.map
 
+		print('\nlength : '+str(self.length)+', width : '+str(self.width)+', vision : '+str(self.vision))		
+		
 		obs=np.concatenate([big_map.reshape(2000,),self.length.reshape(1,),self._pos.reshape(2,),self._vel.reshape(2,)])
 		return obs
 
-	def _render(self):
+	def render(self,mode='human'):
 		pass
 
-	def _seed(self):
+	def seed(self):
 		pass
 
-	def _close(self):
+	def close(self):
 		pass
 
 	def addSquares(self,x, y0, y1, squares):
@@ -177,28 +190,28 @@ class SamuraiEnv(gym.Env):
 				squares.append(np.array([x, y]))
 	def _movable(self,_pos,next_pos): #移動可能判定
 		r = []
-		if next_pos[0] == self._pos[0]:
-			self.addSquares(self._pos[0], self._pos[1], next_pos[1], r)
+		if next_pos[0] == _pos[0]:
+			self.addSquares(_pos[0], _pos[1], next_pos[1], r)
 		else:
-			a = (next_pos[1] - self._pos[1]) / (next_pos[0] - self._pos[0])
-			sgnx = 1 if self._pos[0] < next_pos[0] else -1
-			y1 = a * sgnx / 2.0 + self._pos[1] + 0.5
-			iy1 = (math.floor(y1) if next_pos[1] > self._pos[1] else math.ceil(y1) - 1)
-			self.addSquares(self._pos[0], self._pos[1], iy1, r)
-			for x in range(self._pos[0] + sgnx, next_pos[0], sgnx):
-				y0 = a * (x - self._pos[0] - sgnx / 2) + self._pos[1] + 0.5
-				y1 = a * (x - self._pos[0] + sgnx / 2) + self._pos[1] + 0.5
-				if next_pos[1] > self._pos[1]:
+			a = (next_pos[1] - _pos[1]) / (next_pos[0] - _pos[0])
+			sgnx = 1 if _pos[0] < next_pos[0] else -1
+			y1 = a * sgnx / 2.0 + _pos[1] + 0.5
+			iy1 = (math.floor(y1) if next_pos[1] > _pos[1] else math.ceil(y1) - 1)
+			self.addSquares(_pos[0], _pos[1], iy1, r)
+			for x in range(_pos[0] + sgnx, next_pos[0], sgnx):
+				y0 = a * (x - _pos[0] - sgnx / 2) + _pos[1] + 0.5
+				y1 = a * (x - _pos[0] + sgnx / 2) + _pos[1] + 0.5
+				if next_pos[1] > _pos[1]:
 					iy0, iy1 = math.ceil(y0) - 1, math.floor(y1)
 				else:
 					iy0, iy1 = math.floor(y0), math.ceil(y1) - 1
 				self.addSquares(x, iy0, iy1, r)
-			y0 = a * (next_pos[0] - self._pos[0] - sgnx / 2) + self._pos[1] + 0.5
-			iy0 = (math.ceil(y0) - 1 if next_pos[1] > self._pos[1] else math.floor(y0))
+			y0 = a * (next_pos[0] - _pos[0] - sgnx / 2) + _pos[1] + 0.5
+			iy0 = (math.ceil(y0) - 1 if next_pos[1] > _pos[1] else math.floor(y0))
 			self.addSquares(next_pos[0], iy0, next_pos[1], r)
 
-		return (0 <= self._pos[0] < self.width 
-				and not any(0 <= s[1] and s[0] < self.length and
+		return (0 <= next_pos[0] < self.width 
+				and not any(0 <= s[1] and s[1] < self.length and 
                                 self.map[s[1]][s[0]] == 1 for s in r))
 
     
@@ -206,13 +219,15 @@ class SamuraiEnv(gym.Env):
 		if next_pos[1] < self.length:
 			return False
         
-		dx = (next_pos[0] - self._pos[0])
-		a = (next_pos[1] - self._pos[1]) / dx #傾き
-		y = math.floor(dx * a) + self._pos[1]
+		dx = (next_pos[0] - _pos[0])
+		if not dx==0:
+			a = (next_pos[1] - _pos[1]) / dx #傾き
+			y = math.floor(dx * a) + _pos[1]
+		y=self.length
         
 		while y <= self.length - 1:
 			dx = dx - 1
-			y = math.floor(dx * a) + self._pos[1]
-		next_pos2 = [self._pos[0] + dx, y]
+			y = math.floor(dx * a) + _pos[1]
+		next_pos2 = [_pos[0] + dx, y]
         
-		return next_pos[0] < self.width and self.movable(self._pos,next_pos2) 
+		return 0<=next_pos[0] < self.width and self._movable(_pos,next_pos2) 
