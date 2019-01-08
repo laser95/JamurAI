dc2adbc4e066cf3048ae0f5f021a33d836d6a808
Takuma YAMAUCHI <s1810187@lin01.jaist.ac.jp>
2019-01-08 21:32:20 +0900

    revert EpsGreedyQPolicy, and create SamurAIPolicy

--- SamurAI18-19/LearningEnvironment/keras-rl/build/lib/rl/policy.py
+++ SamurAI18-19/LearningEnvironment/keras-rl/build/lib/rl/policy.py
@@ -259,7 +259,7 @@ class Candidate(object):
                  self.state.velocity.y,
                  (0 if self.from_ is None else self.from_.seq)))
 
-class EpsGreedyQPolicy(Policy):
+class SamurAIPolicy(Policy):
     """Implement the epsilon greedy policy
 
     Eps Greedy policy either:
@@ -268,7 +268,7 @@ class EpsGreedyQPolicy(Policy):
     - takes current best action with prob (1 - epsilon)
     """
     def __init__(self, eps=.1):
-        super(EpsGreedyQPolicy, self).__init__()
+        super(SamurAIPolicy, self).__init__()
         self.eps = eps
 
     def select_action(self, q_values, observation=None):
@@ -410,6 +410,46 @@ class EpsGreedyQPolicy(Policy):
         config['eps'] = self.eps
         return config
 
+class EpsGreedyQPolicy(Policy):
+    """Implement the epsilon greedy policy
+
+    Eps Greedy policy either:
+
+    - takes a random action with probability epsilon
+    - takes current best action with prob (1 - epsilon)
+    """
+    def __init__(self, eps=.1):
+        super(EpsGreedyQPolicy, self).__init__()
+        self.eps = eps
+
+    def select_action(self, q_values, observation=None):
+        """Return the selected action
+
+        # Arguments
+            q_values (np.ndarray): List of the estimations of Q for each action
+
+        # Returns
+            Selection action
+        """
+        assert q_values.ndim == 1
+        nb_actions = q_values.shape[0]
+
+        if np.random.uniform() < self.eps:
+	    action = np.random.random_integers(0, nb_actions-1)
+	    
+        else:
+            action = np.argmax(q_values)
+        return action
+
+    def get_config(self):
+        """Return configurations of EpsGreedyQPolicy
+
+        # Returns
+            Dict of config
+        """
+        config = super(EpsGreedyQPolicy, self).get_config()
+        config['eps'] = self.eps
+        return config
 
 class GreedyQPolicy(Policy):
     """Implement the greedy policy
