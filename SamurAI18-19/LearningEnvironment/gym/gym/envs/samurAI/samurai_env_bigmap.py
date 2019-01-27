96a55f1465e107aa1a2faacfb420614975b803ba
GOM9 <monoone.factory5@gmail.com>
2019-01-27 21:34:09 +0900

    change bigmap

--- SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env_bigmap.py
+++ SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/samurai_env_bigmap.py
@@ -10,7 +10,7 @@ import math
 import matplotlib.pyplot as plt
 
 # マップ上のjockyの加速度を操作として、ゴールに移動させることを目標とする環境
-class SamuraiEnv(gym.Env):
+class SamuraiEnvBigmap(gym.Env):
 
 
 	def __init__(self):
@@ -18,19 +18,8 @@ class SamuraiEnv(gym.Env):
 		# 加速度9種(-1~1,-1~1)
 		self.action_space = spaces.Discrete(9)
 
-		#観測空間(俺らが見たい世界...jockyのmapとlengthとpositionとvelocity)
-		#mapの値域は[-3,2]
-		#0->None , 1->Obstacle, 2->Puddle, -1->Unknown, -2->Behind Goal, -3->Outside Course
-		#positionの上限はルール通り19,99
-		#下限は0,-無限だが、とりあえず、0,-10で
-		#速度の上限、下限はとりあえず10,-10で(ステージごとに計算したほうがいい？)
-		#map,position,velocityそれぞれで値域が異なるのでDictionaryを使って定義
-		self.observation_space=spaces.Dict({
-			"map" : spaces.Box(-3,2,shape=(20,100),dtype="int32"),
-			"length" : spaces.Box(50,100,shape=(1,),dtype="int32"),
-			"position" : spaces.Box(np.array([0,-10]),np.array([19,99]),dtype="int32"),
-			"velocity" : spaces.Box(np.array([-10,-10]),np.array([10,10]),dtype="int32")
-		})
+		# 観測空間
+		self.observation_space = spaces.Box(low=0, high=1, shape=(8, 30, 20), dtype=np.uint8)
 		self.startX=[0]*2
 
 		#for plot
@@ -82,7 +71,7 @@ class SamuraiEnv(gym.Env):
 		
 		done = self._is_done(self._pos,next_pos)
 
-		pre_pos=self._pos
+		pre_pos = self._pos
 		if self._movable(self._pos,next_pos): 
 			if next_pos[1]<0 or (not done and self.map[next_pos[1]][next_pos[0]] == 0):
 				self._vel = next_vel
@@ -94,8 +83,7 @@ class SamuraiEnv(gym.Env):
 			self._vel = np.array([0,0])
 			self._pos = self._pos
 
-
-	    	#ゴールしたら1,それ以外はゴールにy軸
+		#ゴールしたら1,それ以外はゴールにy軸
 		if done:
 			reward = 100.0
 			print('\n\n\n!!!!!!goolgoolgool!!!!\n\n\n')
@@ -106,13 +94,14 @@ class SamuraiEnv(gym.Env):
 
 
 		#取り得る最大のマップを定義, 全て1(壁)で指定
-		big_map = np.empty((100, 20)) 
+		big_map = np.empty((100, 20))
+
 		#指定されたマップを-1(未知)で指定
 		big_map[0:self.length, 0:self.width] = -1
 		#指定されたマップのy軸の範囲外を-2(ゴール以降)で指定
 		big_map[self.length:100, 0:self.width] = -2
 		#指定されたマップ範囲外を-3(コース外)で指定
-		big_map[:, self.width:20] = -3  
+		big_map[:, self.width:20] = -3
 		# ゴールするまで、毎stepでbig_mapを取得
 		if self._pos[1] < self.length:
 			vision = self._pos[1] + self.vision
@@ -124,7 +113,29 @@ class SamuraiEnv(gym.Env):
 				big_map[0:vision, 0:self.width] = self.map[0:vision,:]
 
 		if self.step_limit <= self._step:
-			done=True
+			done = True
+
+		layer_map = np.zeros((8, 100, 20)).astype(np.uint8)
+		'''
+			0: Outside of course (-3)
+			1: Behind of goal (-2)
+			2: Unknown (-1)
+			3: None (0)
+			4: Obstacle (1)
+			5: Puddle (2)
+			6: Position
+			7: Previous position
+		'''
+		layer_map[0, :, :] = np.where(big_map == -3, 1, 0)
+		layer_map[1, :, :] = np.where(big_map == -2, 1, 0)
+		layer_map[2, :, :] = np.where(big_map == -1, 1, 0)
+		layer_map[3, :, :] = np.where(big_map == 0, 1, 0)
+		layer_map[4, :, :] = np.where(big_map == 1, 1, 0)
+		layer_map[5, :, :] = np.where(big_map == 2, 1, 0)
+		if self._pos[1] >= 0:
+			layer_map[6, self._pos[1], self._pos[0]]=1
+		if pre_pos[1] >= 0:
+			layer_map[7, pre_pos[1], pre_pos[0]] = 1
 
 		#for plot
 		self.reward_stock += reward
@@ -132,13 +143,12 @@ class SamuraiEnv(gym.Env):
 		
 
 		#return {"map" : big_map, "length" : self.length, "position" : self._pos, "velocity" : self._vel},reward,done,{}
-		return np.concatenate([big_map.reshape(2000,),self.length.reshape(1,),self._pos.reshape(2,),self._vel.reshape(2,)]),reward,done,{}
-
+		return layer_map, reward, done, {}
 
 	def reset(self):
 		#コースを生成
 		generator_path = "course_generator/course_generator.py"
-		subprocess.call("python3 %s > course/course.crs" % generator_path,shell=True)
+		subprocess.call("python3 %s > course/course.crs" % generator_path, shell=True)
 
 		course_path = "course/course.crs"
 		json_file = open(course_path, 'r')
@@ -146,7 +156,7 @@ class SamuraiEnv(gym.Env):
 		if not json_obj['filetype'] == 'race course 2018':
 			print("The input file does not contain race course data")
 			sys.exit(1)
-  		 
+
 		self.width = json_obj['width']
 		self.length = json_obj['length']
 		self.vision = json_obj['vision']
@@ -164,39 +174,64 @@ class SamuraiEnv(gym.Env):
 		self._vel = np.array([0,0])
 		
 		{
-			"map" : spaces.Box(-3,2,shape=(20,100),dtype="int32"),
-			"length" : spaces.Box(50,100,shape=(1,),dtype="int32"),
-			"position" : spaces.Box(np.array([0,-10]),np.array([19,99]),dtype="int32"),
-			"velocity" : spaces.Box(np.array([-10,-10]),np.array([10,10]),dtype="int32")
+			"map": spaces.Box(-3, 2, shape=(20, 100), dtype="int32"),
+			"length": spaces.Box(50, 100, shape=(1,), dtype="int32"),
+			"position": spaces.Box(np.array([0, -10]), np.array([19, 99]), dtype="int32"),
+			"velocity": spaces.Box(np.array([-10, -10]), np.array([10, 10]), dtype="int32")
 		}
 
 		#return {"map" : self.map, "length" : self.length, "position" : self._pos, "velocity" : self._vel}
 
 		big_map = np.empty((100, 20))
 		#指定されたマップのy軸の範囲外を-2(ゴール以降)で指定
-		big_map[self.length:100, 0:20] = -2	
+		big_map[self.length:100, 0:20] = -2
+
 		#指定されたマップ範囲外を-3(コース外)で指定
 		big_map[:, self.width:20] = -3
+
 		vision = self._pos[1] + self.vision
 		big_map[0:vision, 0:self.width] = self.map[0:vision,:]
 		big_map[0:self.length,0:self.width]=self.map
 
+		# コースジェネレーターから取得したコースの情報を出力
 		print('\nlength : '+str(self.length)+', width : '+str(self.width)+', vision : '+str(self.vision))
 
+		# create binary map
+		layer_map = np.zeros((8, 100, 20))
+		'''
+			0: Outside of course (-3)
+			1: Behind of goal (-2)
+			2: Unknown (-1)
+			3: None (0)
+			4: Obstacle (1)
+			5: Puddle (2)
+			6: Position
+			7: Previous position
+		'''
+		layer_map[0, :, :] = np.where(big_map == -3, 1, 0)
+		layer_map[1, :, :] = np.where(big_map == -2, 1, 0)
+		layer_map[2, :, :] = np.where(big_map == -1, 1, 0)
+		layer_map[3, :, :] = np.where(big_map == 0, 1, 0)
+		layer_map[4, :, :] = np.where(big_map == 1, 1, 0)
+		layer_map[5, :, :] = np.where(big_map == 2, 1, 0)
+		layer_map[6, self._pos[1], self._pos[0]] = 1
+		layer_map[7, self._pos[1], self._pos[0]] = 1
+
+		np.set_printoptions(threshold=np.inf)
+		print('big map:\n', big_map)
+		print('layer map:\n', layer_map)
 
 		#plot
 		self.reward_plot.append(self.reward_stock)
 		self.x_plot.append(len(self.reward_plot))
-		plt.plot(self.x_plot,self.reward_plot,color='blue')
+		plt.plot(self.x_plot, self.reward_plot, color='blue')
 		plt.title("Learning curve")
 		plt.xlabel("Episode")
 		plt.ylabel("Reward")
-		plt.pause(0.1)
+		# plt.pause(0.1)
 		self.reward_stock = 0
-	
-		
-		obs=np.concatenate([big_map.reshape(2000,),self.length.reshape(1,),self._pos.reshape(2,),self._vel.reshape(2,)])
-		return obs
+
+		return layer_map
 
 	def render(self,mode='human',close=False):
 		outmap = self.map.copy()
@@ -205,8 +240,8 @@ class SamuraiEnv(gym.Env):
 		outfile = sys.stdout
 		velostr = 'velocity.x:' + str(self._vel[0]) + '\nvelocity.y:' + str(self._vel[1]) + '\n'
 		mapstr = '\n'.join(''.join(
-                str(value) for value in row
-                ) for row in outmap
+				str(value) for value in row
+				) for row in outmap
             ) + '\n'
 		outfile.write(velostr + mapstr)
 		return outfile	
@@ -250,20 +285,21 @@ class SamuraiEnv(gym.Env):
 				and not any(0 <= s[1] and s[1] < self.length and 
                                 self.map[s[1]][s[0]] == 1 for s in r))
 
-    
 	def _is_done(self,_pos,next_pos):
 		if next_pos[1] < self.length:
 			return False
-        
+
 		dx = (next_pos[0] - _pos[0])
 		if not dx==0:
 			a = (next_pos[1] - _pos[1]) / dx #傾き
 			y = math.floor(dx * a) + _pos[1]
 		y=self.length
-        
+
 		while y <= self.length - 1:
 			dx = dx - 1
 			y = math.floor(dx * a) + _pos[1]
 		next_pos2 = [_pos[0] + dx, y]
-        
-		return 0<=next_pos[0] < self.width and self._movable(_pos,next_pos2) 
+
+		return 0<=next_pos[0] < self.width and self._movable(_pos,next_pos2)
+
+
