613b0467314279c5cce363f2bcad9bf6ee117493
Takuma YAMAUCHI <s1810187@lin01.jaist.ac.jp>
2019-02-03 13:06:49 +0900

    modify import path of gym

--- SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/__init__.py
+++ SamurAI18-19/LearningEnvironment/gym/gym/envs/samurAI/__init__.py
@@ -1,2 +1,4 @@
-from gym.envs.samurAI.samurai_env import SamuraiEnv, SamuraiEnvBigmap, SamuraiEnvCropmap
+from gym.envs.samurAI.samurai_env import SamuraiEnv
+from gym.envs.samurAI.samurai_env_bigmap import SamuraiEnvBigmap
+from gym.envs.samurAI.samurai_env_cropmap import SamuraiEnvCropmap
 
